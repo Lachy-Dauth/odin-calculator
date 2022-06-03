f901ebf5e7a0f61843360c2c8ab020c3964b3efd
Lachlan Dauth <lachlandauth@outlook.com>
2022-06-03 21:12:22 +0800

    Calc done

--- index.js
+++ index.js
@@ -36,10 +36,86 @@ function operate(operation, a, b) {
     case "×":
       return multiply(a, b);
     case "÷":
-      return b ? null : divide(a, b); // if statement is to prevent a division by zero error
+      return b ? divide(a, b) : null; // if statement is to prevent a division by zero error
     case "^":
       return power(a, b);
   }
   return null;
 }
 
+let prevNum = "";
+let prevOp = "";
+let operator = "";
+let num = "0";
+
+const numbers = document.querySelectorAll(".number");
+const operations = document.querySelectorAll(".operator");
+const del = document.querySelector("#delete");
+const clr = document.querySelector("#clear");
+const dec = document.querySelector(".decimal");
+const lower = document.querySelector(".lower");
+const upper = document.querySelector(".upper");
+const equ = document.querySelector(".equals");
+
+numbers.forEach(btn => {
+  btn.addEventListener("click", e => {
+    num = +num || num.length > 1 ? num += e.target.dataset.number : e.target.dataset.number;
+    lower.textContent = Math.round(+num * 100000) / 100000;
+  })
+})
+
+del.addEventListener("click", e => {
+  num = num.replace(/.$/, '');
+  if (num.length == 0) num = "0";
+  lower.textContent = Math.round(+num * 100000) / 100000;
+})
+
+clr.addEventListener("click", e => {
+  prevNum = "";
+  prevOp = "";
+  operator = "";
+  num = "0";
+  upper.textContent = "";
+  lower.textContent = Math.round(+num * 100000) / 100000;
+})
+
+equ.addEventListener("click", e => {
+  let temp = Math.round(+prevNum * 100000) / 100000;
+  if (prevNum === "") {
+    prevNum = num;
+    num = "";
+  }
+  else if (num !== "" && operator !== "") {
+    num = operate(operator, prevNum, num);
+    lower.textContent = Math.round(+num * 100000) / 100000;
+    upper.textContent = `${temp} ${operator} ${Math.round(+prevNum * 100000) / 100000} =`;
+    prevNum = "";
+    prevOp = "";
+    operator = "";
+  }
+})
+
+dec.addEventListener("click", e => {
+  if (num.split(".").length == 2) {
+    return;
+  }
+  if (num.length == 0) num = "0";
+  num += ".";
+  lower.textContent = Math.round(+num * 100000) / 100000;
+})
+
+operations.forEach(btn => {
+  btn.addEventListener("click", e => {
+    if (prevNum === "") {
+      prevNum = num;
+      num = "";
+    }
+    else if (num !== "" && operator !== "") {
+      prevNum = operate(operator, prevNum, num);
+      num = "";
+      lower.textContent = Math.round(+prevNum * 100000) / 100000;
+    }
+    operator = e.target.dataset.op;
+    upper.textContent = `${Math.round(+prevNum * 100000) / 100000} ${operator}`;
+  })
+})
